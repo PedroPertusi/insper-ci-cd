b11118f8d614ebb2cea1a88dda6302fa33711406
PedroPertusi <pedrovazpertusi@gmail.com>
2023-10-04 14:59:08 -0300

    testes time

--- src/test/java/com/insper/partida/equipe/TeamControllerTests.java
+++ src/test/java/com/insper/partida/equipe/TeamControllerTests.java
@@ -1,6 +1,7 @@
 package com.insper.partida.equipe;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.insper.partida.equipe.dto.SaveTeamDTO;
 import com.insper.partida.equipe.dto.TeamReturnDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -39,7 +41,6 @@ public class TeamControllerTests {
 
     @Test
     void test_listTeams() throws Exception {
-
         TeamReturnDTO team = new TeamReturnDTO();
         team.setIdentifier("time-1");
 
@@ -57,11 +58,49 @@ public class TeamControllerTests {
 
         String resp = result.getResponse().getContentAsString();
         Assertions.assertEquals(om.writeValueAsString(times), resp);
-
     }
 
+    @Test
+    void test_saveTeam() throws Exception {
+        SaveTeamDTO saveTeamDTO = new SaveTeamDTO();
+        saveTeamDTO.setName("Team 1");
+        saveTeamDTO.setIdentifier("team-1");
 
+        TeamReturnDTO teamReturnDTO = new TeamReturnDTO();
+        teamReturnDTO.setIdentifier("team-1");
+        teamReturnDTO.setName("Team 1");
 
+        Mockito.when(teamService.saveTeam(Mockito.any(SaveTeamDTO.class))).thenReturn(teamReturnDTO);
 
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/team")
+                        .content(asJsonString(saveTeamDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn();
 
+        ObjectMapper om = new ObjectMapper();
+
+        String resp = result.getResponse().getContentAsString();
+        Assertions.assertEquals(om.writeValueAsString(teamReturnDTO), resp);
+    }
+
+    @Test
+    void test_deleteTeam() throws Exception {
+        String identifier = "team-1";
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/team/{identifier}", identifier))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        Mockito.verify(teamService, Mockito.times(1)).deleteTeam(identifier);
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/com/insper/partida/equipe/TeamServiceTests.java
+++ src/test/java/com/insper/partida/equipe/TeamServiceTests.java
@@ -1,6 +1,8 @@
 package com.insper.partida.equipe;
 
+import com.insper.partida.equipe.dto.SaveTeamDTO;
 import com.insper.partida.equipe.dto.TeamReturnDTO;
+import com.insper.partida.equipe.exception.TeamAlreadyExistsException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,7 +23,6 @@ public class TeamServiceTests {
     @Mock
     TeamRepository teamRepository;
 
-
     @Test
     void test_listTeams() {
         Mockito.when(teamRepository.findAll()).thenReturn(new ArrayList<>());
@@ -33,7 +34,6 @@ public class TeamServiceTests {
 
     @Test
     void test_listTeamsNotEmpty() {
-
         Team team = getTeam();
 
         List<Team> lista = new ArrayList<>();
@@ -45,18 +45,52 @@ public class TeamServiceTests {
 
         Assertions.assertEquals(1, resp.size());
     }
+    
+    @Test
+    void test_saveTeamAlreadyExists() {
+        SaveTeamDTO saveTeamDTO = new SaveTeamDTO();
+        saveTeamDTO.setName("Team 3");
+        saveTeamDTO.setIdentifier("team-3");
 
+        Mockito.when(teamRepository.existsByIdentifier(saveTeamDTO.getIdentifier())).thenReturn(true);
 
+        Assertions.assertThrows(TeamAlreadyExistsException.class, () -> {
+            teamService.saveTeam(saveTeamDTO);
+        });
+    }
 
+    @Test
+    void test_deleteTeam() {
+        String identifier = "team-1";
 
+        Team team = getTeam();
+
+        Mockito.when(teamRepository.findByIdentifier(identifier)).thenReturn(team);
+
+        teamService.deleteTeam(identifier);
+
+        Mockito.verify(teamRepository, Mockito.times(1)).delete(team);
+    }
+
+    @Test
+    void test_getTeam() {
+        String identifier = "team-1";
+
+        Team team = getTeam();
+
+        Mockito.when(teamRepository.findByIdentifier(identifier)).thenReturn(team);
+
+        TeamReturnDTO resp =  TeamReturnDTO.covert(teamService.getTeam(identifier));
+
+        Assertions.assertNotNull(resp);
+        Assertions.assertEquals("Time 1", resp.getName());
+    }
 
     private static Team getTeam() {
         Team team = new Team();
         team.setId("1");
-        team.setIdentifier("time-1");
+        team.setIdentifier("team-1");
         team.setName("Time 1");
         return team;
     }
-
-
 }
